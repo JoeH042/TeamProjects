5e32ce42283a0244d3e98a9fb6627e55a29086be
w079yxw <yipeng.wang3000@gmail.com>
2016-09-30 00:06:10 -0400

    Update TM_Member_Of_Grp.php

--- TM_Member_Of_Grp.php
+++ TM_Member_Of_Grp.php
@@ -14,12 +14,7 @@
 <p>Group ID:
 <input type="text" name="g_id" size="30" value="" />
 </p>
-<p>Member Starting Date(d/m/y):
-<input type="text" name="m_startda" size="30" value="" />
-</p>
-<p>Member Ending Date(d/m/y):
-<input type="text" name="m_endda" size="30" value="" />
-</p>
+
 <p>
 <input type="submit" name="submit" value="Send" />
 </p>
@@ -30,25 +25,18 @@
 </body>
 </html>
 <?php
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
     if($_SERVER ['REQUEST_METHOD']=='POST')//check if user submit the form
 {
     
     
-
     $M_id = $_POST['m_id'];
     $G_id = $_POST['g_id'];
-    $M_startD = $_POST['m_endda'];
-    $M_endD = $_POST['m_endda'];
-     //unset($id,$Firstname,$Middlename,$Lastname,$Email,$PhoneNumber,$Status,$Department,$TeamNumber);
-
-   if(!empty($M_id)&&!empty($G_id)&&!empty($M_startD)&&!empty($M_endD)){
+    if(!empty($M_id)&&!empty($G_id)){
         //check if value filled or not
        echo "hello";
        $connect = mysql_connect("127.0.0.1","root","");
@@ -56,7 +44,7 @@
         echo "table  is  created!";
         //link form var value into database table
         
-       $sql="INSERT INTO TM_Member_Of_Grp (Member_ID,Group_ID,Member_StartingDate,Member_EndingDate) VALUES ('$M_id','$G_id','$M_startD','$M_endD')";
+       $sql="INSERT INTO TM_Member_Of_Grp (Member_ID,Group_ID) VALUES ('$M_id','$G_id')";
          
         mysql_query($sql,$connect);
          header("Location: TM_Member_Of_Grp.php");
@@ -75,15 +63,12 @@ else
     echo"Please complete the form";
     
 }
-
 ?>
 
 
 <?php
-
 $connect = mysql_connect("127.0.0.1","root","");
 $db = mysql_select_db("CEG4981",$connect);
-
 echo '<table align="left"
 cellspacing="5" cellpadding="8">
 <tr>
@@ -105,5 +90,3 @@ while($row = mysql_fetch_array($r)){
 }
 echo '</table>';
 ?>
-
-
