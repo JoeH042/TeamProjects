99a2574ab45d32815a389b5804bf5576fdad2085
Zuli Rhodes <guilarzm@gmail.com>
2016-10-26 15:42:03 -0400

    Intermediate commit containing Joe's send messages html/css code. Need formatting.

--- view/messages/send_messages_view.php
+++ view/messages/send_messages_view.php
@@ -5,8 +5,29 @@
     include 'view/uniform/header.php';
 ?> 
 
-<main>
-    <h1>Send Messages To be implemented </h1>
+<main class="sendMess">
+    <div class="wrapper">	
+
+        <div class="form-con">
+
+            <h1>Send A Message</h1>
+
+            <form class="form row" name="form" method="POST" action="">
+                    <div class="col-6">
+                    <label>To: </label><input type="text" placeholder="Input Group Names (Separate by ,)" />
+                    <textarea placeholder="Message Text"></textarea>
+                </div>
+
+                    <div class="col-6">
+                    <label>Message will be sent To: (X Users)</label>
+                    <label>Message will be sent To: (Time Stamp)</label>
+                    <input type="submit" value="Submit" class="button" />
+                </div>
+            </form>
+
+        </div>
+    </div>
+
 </main>
 
 <?php include 'view/uniform/footer.php'; ?>
